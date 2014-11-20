8843853607e82207ac46c15b578ce71f36d75cbd
rsgoss <rsgoss@gmail.com>
2014-11-19 17:41:56 -0700

    Updated per Lab12

--- facade.py
+++ facade.py
@@ -11,11 +11,19 @@ class Computer:
         self.hd=parts.HardDisk()
     # implement the class below this line    
 
+    def startComputer(self):
+        self.cpu.check();
+        self.mem.load();
+        self.hd.mount();
 
+    def printCPUInfo(self):
+        self.cpu.getInfo();
 
+    def printMemInfo(self):
+        self.mem.getInfo();
 
-
-
+    def printHDInfo(self):
+        self.hd.getInfo();
 
 
 # The running part
